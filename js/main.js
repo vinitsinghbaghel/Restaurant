488cda0c7969feffc45cf6ca2c57ecdad17b33f0
vinitsinghbaghel <baghel0504@gmail.com>
2020-10-07 18:27:15 +0530

    Review about food

---
+++ js/main.js
@@ -0,0 +1,39 @@
+// Local Reviews
+const reviews = [
+    {
+      id: 1,
+      name: "Sanjeev Kapoor",
+      job: "CEO, Khana Khajana",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+      text:
+      "Visited as a guest in the Echo restaurant for lunch just today.We were entertaining friends from California, and enjoyed our ocean side table.We chose to stay indoors - to enjoy the air conditioning.",
+    },
+    {
+      id: 2,
+      name: "Emma Watson",
+      job: "CTO, XYZ Company",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+      text:
+        "We had lunch here a few times while on the island visiting family and friends. The servers here are just wonderful and have great memories it seems.",
+    },
+    {
+      id: 3,
+      name: "Felicity Smoke",
+      job: "Intern",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+      text:
+        "The beach views, calm ocean and ability to watch the sunset on our patio were highlights of the trip! The hotel restaurant was exceptional, not only because every meal we enjoyed had a variety of options and was flavorful but also because the prices were so reasonable",
+    },
+    {
+      id: 4,
+      name: "Oliver Queen",
+      job: "Owner, ABC Company",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+      text:
+        "I had lunch with some of my colleagues at Echo on Day 1. I had the wedge salad - it was delicious. On Night 2, I enjoyed a drink at the bar. I had a Margarita. The service was excellent. ",
+    },
+  ];
